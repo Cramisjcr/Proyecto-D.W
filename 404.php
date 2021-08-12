10de0cd8c636c6a88afedfde4e15d76b0a0d5412
cramisjcr <cramirezmv@gmail.com>
2021-08-11 21:18:29 -0500

    Primer Commit. Creando Repo

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+</head>
+<body>
+    <div class="container_404">
+        <h1>Error 404</h1>
+        <p>Esta página no se encuentra
+            disponible en estos momentos. Disculpa las molestias
+        </p>
+        <a href="/index.html" class="boton">Página Principal</a>
+
+    </div>
+    
+</body>
+</html>
